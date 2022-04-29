4c88ac0e21433602bfda30380898d0babcce3036
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-04-29 15:30:49 +0200

    dasda

--- system-templates/toasts-user.php
+++ system-templates/toasts-user.php
@@ -30,7 +30,7 @@ $page_title = $wp_query->post->post_title;
 <script>
 jQuery(document).ready(function($){
     var ajax_url = "<?= admin_url('admin-ajax.php'); ?>";
-    var formData = "";
+    var formData = $('form[name=masterdata-form]');
  
     /*setInterval(function(){
     load_last_notification();
