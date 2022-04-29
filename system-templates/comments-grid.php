15d2da71165b81cff027e9747ac442a0a5d535df
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-04-29 03:21:59 +0200

    input

--- system-templates/comments-grid.php
+++ system-templates/comments-grid.php
@@ -233,7 +233,7 @@ $user_id = $current_user_id = get_current_user_id();
 							</button>
 						</div>
 						<div class="col-md-10 col-8 m-0 p-0">
-							<input type="text" maxlength="120" placeholder="Etwas mitteilen..." pattern="[^()/><\][\\\x22,;|]@+" required>
+							<input type="text" maxlength="120" placeholder="Etwas mitteilen..." pattern="[^()/><\][\\\x22,;|]@+">
 						</div>
 						<div class="col-md col-2 chat-submit">
 							<button class="bg-dark text-light" data-toggle="tooltip" data-placement="top"
