b5f09aa6c13285532511721f9d13430a9ca64e05
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-02 20:09:37 +0200

    da

--- loop-templates/content-benefits.php
+++ loop-templates/content-benefits.php
@@ -6,28 +6,28 @@
  */
 
 if ( $args['data'] ) {
-    $benefits_class = $args['class'];
-    $benefits_icon = $args['data']['icon'];
-    $benefits_headline = $args['data']['headline'];
-    $benefits_text = $args['data']['text'];
+    $benefitsClass = $args['class'];
+    $benefitsIcon = $args['data']['icon'];
+    $benefitsHeadline = $args['data']['headline'];
+    $benefitsText = $args['data']['text'];
 }
 ?>
 
-<div class="<?php echo $benefits_class ?>">
+<div class="<?php echo $benefitsClass ?>">
     <?php if( have_rows('landingpageBenefitsArray') ): $c = 0; $class = ''; ?>
         <?php while( have_rows('landingpageBenefitsArray') ) : the_row(); ?>
             <?php $c++; ?>
                 <div class="col-md-4 px-md-3 py-md-0 my-3">
                     <span class="negative-margin fa-stack fa-3x">
-                        <i class="<?php echo $benefits_icon ?> fa-stack-2x text-primary"></i>
+                        <i class="<?php echo $benefitsIcon ?> fa-stack-2x text-primary"></i>
                     </span>
                     <div class="card px-0 mx-3 box-shadow-lg">
                         <div class="card-header">
                             <br>
-                            <h4><?php echo $benefits_headline ?></h4>
+                            <h4><?php echo $benefitsHeadline ?></h4>
                         </div>
                         <div class="card-body mb-0 border-0">
-                            <p class="text-muted"><?php echo $benefits_text ?></p>
+                            <p class="text-muted"><?php echo $benefitsText ?></p>
                         </div>
                     </div>
                 </div>
