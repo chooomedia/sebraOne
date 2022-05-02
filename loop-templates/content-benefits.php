1aee740a8b23a30fa209b7ce2820a09c96f7bb70
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-02 20:22:02 +0200

    ff

--- loop-templates/content-benefits.php
+++ loop-templates/content-benefits.php
@@ -7,30 +7,39 @@
 
 if ( $args['data'] ) {
     $benefitsClass = $args['class'];
-    $benefitsIcon = $args['data']['icon'];
     $benefitsHeadline = $args['data']['headline'];
-    $benefitsText = $args['data']['text'];
+    $benefitsFootline = $args['data']['footline'];
 }
 ?>
-
-<div class="<?php echo $benefitsClass ?>">
-    <?php if( have_rows('landingpageBenefitsArray') ): $c = 0; $class = ''; ?>
-        <?php while( have_rows('landingpageBenefitsArray') ) : the_row(); ?>
-            <?php $c++; ?>
-                <div class="col-md-4 px-md-3 py-md-0 my-3">
-                    <span class="negative-margin fa-stack fa-3x">
-                        <i class="<?php echo $benefitsIcon ?> fa-stack-2x text-primary"></i>
-                    </span>
-                    <div class="card px-0 mx-3 box-shadow-lg">
-                        <div class="card-header">
-                            <br>
-                            <h4><?php echo $benefitsHeadline ?></h4>
-                        </div>
-                        <div class="card-body mb-0 border-0">
-                            <p class="text-muted"><?php echo $benefitsText ?></p>
+<div class="container">
+    <div class="text-center">
+        <h2 class="section-heading text-uppercase mt-md-0"><b><?php echo $benefitsHeadline ?></b></h2><!-- Welche Vorteile? -->
+        <h3 class="section-subheading text-accent mb-5"><?php echo $benefitsFootline ?></h3> <!-- <b>Einfach & fair</b> den <b>besten Verkaufspreis</b> erhalten -->
+    </div>
+    <div class="<?php echo $benefitsClass ?>">
+        <?php if( have_rows('landingpageBenefitsArray') ): $c = 0; $class = ''; ?>
+            <?php while( have_rows('landingpageBenefitsArray') ) : the_row(); ?>
+                 <?php 
+                    $sub_icon = get_field('landingpageBenefitsIcon');
+                    $sub_Headline = get_field('landingpageBenefitsHeadline');
+                    $sub_Text = get_field('landingpageBenefitsText'); 
+                ?>
+                <?php $c++; ?>
+                    <div class="col-md-4 px-md-3 py-md-0 my-3">
+                        <span class="negative-margin fa-stack fa-3x">
+                            <i class="<?php echo $sub_icon ?> fa-stack-2x text-primary"></i>
+                        </span>
+                        <div class="card px-0 mx-3 box-shadow-lg">
+                            <div class="card-header">
+                                <br>
+                                <h4><?php echo $sub_Headline ?></h4>
+                            </div>
+                            <div class="card-body mb-0 border-0">
+                                <p class="text-muted"><?php echo $sub_Text ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-        <?php endwhile; ?>
-    <?php endif; ?>		
+            <?php endwhile; ?>
+        <?php endif; ?>		
+    </div>
 </div>
