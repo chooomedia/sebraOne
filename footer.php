3dcd584dffe35c3a694aa3bd07c41086f4f7accb
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-02 23:09:28 +0200

    da

--- footer.php
+++ footer.php
@@ -20,17 +20,18 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 
 <div id="bug-issue-form-modal-wrapper">
 <!-- Modal -->
-<div class="modal fade" id="bug-issue-form-modal" tabindex="-1" role="dialog" aria-labelledby="bug-issue-form-modal-ModalCenterTitle" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="bug-issue-form-modal-ModalLongTitle">Fehler melden</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<?php echo do_shortcode( '[contact-form-7 id="501" title="bug-issue-form"]' ); ?>
+	<div class="modal fade" id="bug-issue-form-modal" tabindex="-1" role="dialog" aria-labelledby="bug-issue-form-modal-ModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="bug-issue-form-modal-ModalLongTitle">Fehler melden</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<?php echo do_shortcode( '[contact-form-7 id="501" title="bug-issue-form"]' ); ?>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -38,18 +39,19 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 
 <?php if( have_rows('sectionfaqs') ): ?>
 <div id="contact-form-modal-wrapper">
-<!-- Modal -->
-<div class="modal fade" id="contact-issue-form-modal" tabindex="-1" role="dialog" aria-labelledby="contact-issue-form-modal-ModalCenterTitle" aria-hidden="true">
-	<div class="modal-dialog modal-dialog-centered" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="contact-issue-form-modal-ModalLongTitle"><b>Schreiben</b> Sie uns</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<div class="modal-body">
-				<?php echo do_shortcode( '[contact-form-7 id="639" title="contact-corm"]' ); ?>
+	<!-- Modal -->
+	<div class="modal fade" id="contact-issue-form-modal" tabindex="-1" role="dialog" aria-labelledby="contact-issue-form-modal-ModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="contact-issue-form-modal-ModalLongTitle"><b>Schreiben</b> Sie uns</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<?php echo do_shortcode( '[contact-form-7 id="639" title="contact-corm"]' ); ?>
+				</div>
 			</div>
 		</div>
 	</div>
