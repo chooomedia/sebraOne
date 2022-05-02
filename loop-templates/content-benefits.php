296c1f638d7ecd232d920259e003f4f81e5348cc
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-02 21:38:14 +0200

    das

--- loop-templates/content-benefits.php
+++ loop-templates/content-benefits.php
@@ -19,11 +19,6 @@ if ( $args['data'] ) {
     <div class="<?php echo $benefitsClass ?>">
         <?php if( have_rows('benefitsArray') ): $c = 0; $class = ''; ?>
             <?php while( have_rows('benefitsArray') ) : the_row(); ?>
-                <?php 
-                    $sub_icon = the_sub_field('benefits_icon');
-                    $sub_Headline = the_sub_field('benefits_headline');
-                    $sub_Text = the_sub_field('benefits_text'); 
-                ?>
                 <?php $c++; ?>
                     <div class="col-md-4 px-md-3 py-md-0 my-3">
                         <span class="negative-margin fa-stack fa-3x">
@@ -32,7 +27,7 @@ if ( $args['data'] ) {
                         <div class="card px-0 mx-3 box-shadow-lg">
                             <div class="card-header">
                                 <br>
-                                <h4><?php echo the_sub_field('benefits_headline'); ?></h4>
+                                <h4><?php the_sub_field('benefits_headline'); ?></h4>
                             </div>
                             <div class="card-body mb-0 border-0">
                                 <p class="text-muted"><?php the_sub_field('benefits_text') ?></p>
