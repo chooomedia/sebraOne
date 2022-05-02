32f61d3d43f61f4ad12bc07f800df07b38184805
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-03 01:58:00 +0200

    das

--- loop-templates/content-carbrands.php
+++ loop-templates/content-carbrands.php
@@ -9,7 +9,7 @@
 <div class="row align-items-center">
 <?php if( have_rows('carbrandsArray') ): $c = 0; $class = ''; ?>
     <?php while( have_rows('carbrandsArray') ) : the_row(); ?>
-    <?php $image = get_field('carbrandsArray_image'); ?>
+    <?php $image = get_sub_field('carbrandsArray_image'); ?>
 	<div class="col-md-3 col-6 my-3">
 		<a href="#">
 			<img loading="lazy" class="img-fluid img-brand d-block mx-auto" src="<?php $image['url'] ?>" alt="<?php $image['alt'] ?>" />
