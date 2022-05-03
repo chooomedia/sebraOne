c2cfbeeaae657c0f488d73c3cb6b7676f7366b79
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-03 14:24:59 +0200

    agfds

--- chat-templates/user-message.php
+++ chat-templates/user-message.php
@@ -10,7 +10,7 @@
 		</button>
 	</div>
 	<div class="col-md-10 col-8 m-0 p-0">
-		<input id="messageContent" type="text" maxlength="120" onkeyup="responseCheck()" placeholder="Etwas mitteilen...">
+		<input id="messageContent" type="text" maxlength="120" onkeypress="responseCheck()" placeholder="Etwas mitteilen...">
 	</div>
 	<div class="col-md col-2 chat-submit">
 		<!--ADVICE COMMENT-->
