d6dc56030340a56d010ce9268e87dac4c58b40d3
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-03 02:26:59 +0200

    da

--- loop-templates/content-carbrands.php
+++ loop-templates/content-carbrands.php
@@ -13,7 +13,7 @@ if( $images ): ?>
     <?php foreach( $images as $image ): ?>
 	<div class="col-md-3 col-6 my-5">
 		<a href="<?php echo $image['url']; ?>">
-            <img loading="lazy" class="img-fluid img-brand d-block mx-auto" src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+            <img style="filter:greyscale(1);" loading="lazy" class="img-fluid img-brand d-block mx-auto" src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
 		</a>
 	</div>
     <?php endforeach; ?>
