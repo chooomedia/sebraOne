6ba7d0f770ce677b8e98afdc521c53d276ff4de3
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-04 18:11:38 +0200

    das

--- functions.php
+++ functions.php
@@ -243,7 +243,7 @@ function landingpage_custom_post_type() {
 		'filter_items_list'     => __( 'Filtern', 'autoverkaufen' ),
 	);
 	$rewrite = array(
-		'slug'                  => '%custom-taxonomy-name%/auto-verkaufen', 'with_front' => true,
+		'slug'                  => 'auto-verkaufen/%custom-taxonomy-name%', 'with_front' => true,
 		'with_front'            => false,
 		'pages'                 => false,
 		'feeds'                 => false,
@@ -275,16 +275,14 @@ function landingpage_custom_post_type() {
 }
 add_action( 'init', 'landingpage_custom_post_type', 0 );
 
-add_filter('post_type_link', 'landingpage_update_permalink_structure', 10, 2);
-function landingpage_update_permalink_structure( $post_link, $post )
-{
-    if ( false !== strpos( $post_link, '%custom-taxonomy-name%' ) ) {
-        $taxonomy_terms = get_the_terms( $post->ID, 'custom-taxonomy-name' );
-        foreach ( $taxonomy_terms as $term ) { 
-            if ( ! $term->parent ) {
-                $post_link = str_replace( '%custom-taxonomy-name%', $term->slug, $post_link );
-            }
-        } 
+add_filter('post_type_link', 'landingpage_update_permalink_structure', 1, 3);
+function landingpage_update_permalink_structure( $post_link, $id = 0 ){
+    $post = get_post($id);  
+    if ( is_object( $post ) ){
+        $terms = wp_get_object_terms( $post->ID, 'custom-taxonomy-name' );
+        if( $terms ){
+            return str_replace( '%custom-taxonomy-name%' , $terms[0]->slug , $post_link );
+        }
     }
-    return $post_link;
+    return $post_link;  
 }
