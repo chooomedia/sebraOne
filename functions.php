2a798ac74a0c0c6f53b7bd0fbce34c80aca6f7b8
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-04 18:02:21 +0200

    das

--- functions.php
+++ functions.php
@@ -243,7 +243,7 @@ function landingpage_custom_post_type() {
 		'filter_items_list'     => __( 'Filtern', 'autoverkaufen' ),
 	);
 	$rewrite = array(
-		'slug'                  => 'autoverkaufen/%citycategory%',
+		'slug'                  => 'auto-verkaufen/%category%',
 		'with_front'            => false,
 		'pages'                 => false,
 		'feeds'                 => false,
@@ -274,15 +274,3 @@ function landingpage_custom_post_type() {
 
 }
 add_action( 'init', 'landingpage_custom_post_type', 0 );
-
-function landingpage_category_post_link( $post_link, $id = 0 ){
-    $post = get_post($id);  
-    if ( is_object( $post ) ){
-        $terms = wp_get_object_terms( $post->ID, 'autoverkaufen' );
-        if( $terms ){
-            return str_replace( '%citycategory%' , $terms[0]->slug , $post_link );
-        }
-    }
-    return $post_link;  
-}
-add_filter( 'post_type_link', 'landingpage_category_post_link', 1, 3 );
