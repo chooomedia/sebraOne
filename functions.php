02dcc78c47d2c0b553754c3fe65198e2e482d769
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-05-04 18:19:39 +0200

    dasd

--- functions.php
+++ functions.php
@@ -243,7 +243,7 @@ function landingpage_custom_post_type() {
 		'filter_items_list'     => __( 'Filtern', 'autoverkaufen' ),
 	);
 	$rewrite = array(
-		'slug'                  => 'auto-verkaufen',
+		'slug'                  => 'autoverkaufen',
 		'with_front'            => false,
 		'pages'                 => false,
 		'feeds'                 => false,
