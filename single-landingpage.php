2c906103903b7a9c057311e1d8cea87b3499ccd6
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-06-21 02:28:14 +0200

    condi...

--- single-landingpage.php
+++ single-landingpage.php
@@ -97,7 +97,7 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 								Terminanfrage <img width="22px" height="22px" src="<?php echo get_site_icon_url(); ?>" /> SEBRA1
 							</a>
 						
-							<?php if(!the_sub_field('link')) : ?>
+							<?php if(the_sub_field('link')) : ?>
 							<a class="btn btn-primary py-3 px-md-4 px-2" target="_blank" href="<?php the_sub_field('link') ?>">
 								<?php the_sub_field('linktitle') ?>
 							</a>
