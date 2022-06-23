6a908eb3868e331c6d812ce2d57ca298f9ac68c3
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-06-23 15:47:15 +0200

    22

--- page-closingdeal.php
+++ page-closingdeal.php
@@ -16,11 +16,21 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 
 ?>
 <div class="wrapper" id="page-wrapper">
-    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+    <section class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
         <div class="row">
             <main class="site-main container" id="main-closing-deal">
 
             <?php if (is_user_logged_in()) : ?>
+                <?php
+                    while ( have_posts() ) {
+                        the_post();
+
+                        // If comments are open or we have at least one comment, load up the comment template.
+                        if ( comments_open() || get_comments_number() ) {
+                            comments_template();
+                        }
+                    }
+                ?>
                 <?php echo do_shortcode( '[contact-form-7 id="501" title="bug-issue-form"]' ); ?>
             <?php else : ?>
 
@@ -35,5 +45,8 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 
             </main>
         </div> 
-    </div>
+    </section>
 </div>
+
+<?php
+get_footer();
