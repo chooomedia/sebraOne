69fe86fca1350e3333de142e49e56408e0c13a9e
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-06-23 15:44:39 +0200

    added closing deal tempalte only logged in users

---
+++ page-closingdeal.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Closing Deal / Verkaufsabschluss
+ *
+ * Description: Simple template to close deals
+ *
+ * @package SebraOne
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header(); 
+
+$container = get_theme_mod( 'SebraOne_container_type' );
+
+?>
+<div class="wrapper" id="page-wrapper">
+    <div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+        <div class="row">
+            <main class="site-main container" id="main-closing-deal">
+
+            <?php if (is_user_logged_in()) : ?>
+                <?php echo do_shortcode( '[contact-form-7 id="501" title="bug-issue-form"]' ); ?>
+            <?php else : ?>
+
+                <?php printf(
+				'<div id="search-search" class="row vh-50"><p>%s<p>',
+				esc_html__( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'SebraOne' )
+			);
+			get_search_form(array('formId' => 'searchform-search'));
+			printf('</div>'); ?>
+
+            <?php endif; ?>
+
+            </main>
+        </div> 
+    </div>
+</div>
