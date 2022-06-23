d82a780a3cc772e58d06d0795d98e633cf63a1a3
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-06-23 15:55:48 +0200

    das

--- page-closingdeal.php
+++ page-closingdeal.php
@@ -21,14 +21,15 @@ $container = get_theme_mod( 'SebraOne_container_type' );
             <main class="site-main container" id="main-closing-deal">
 
             <?php if (is_user_logged_in()) : ?>
-                <div class="card">
-                    <div class="card-body p-0 p-md-3 mb-5">
+                <div class="card mb-5">
+                    <div class="card-body p-0 p-md-3">
                         <h2><b><?php echo $pageTitle ?></b></h2>
                         <?php the_content() ?>
                     </div>
                 </div>
 
                 <?php echo do_shortcode( '[contact-form-7 id="501" title="bug-issue-form"]' ); ?>
+                
             <?php else : ?>
 
                 <?php printf(
