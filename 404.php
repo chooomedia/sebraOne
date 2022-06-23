3db5960399f7f88a9ed562caa99bd04f62a0d7a9
chooomedia <9967192+chooomedia@users.noreply.github.com>
2022-06-23 20:18:10 +0200

    das

--- 404.php
+++ 404.php
@@ -38,32 +38,6 @@ $container = get_theme_mod( 'SebraOne_container_type' );
 								<?php get_search_form(array('formId' => 'searchform-search')); ?>
 							</div>
 
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( SebraOne_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-
-								<div class="widget widget_categories">
-
-									<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'SebraOne' ); ?></h2>
-
-									<ul>
-										<?php
-										wp_list_categories(
-											array(
-												'orderby'  => 'count',
-												'order'    => 'DESC',
-												'show_count' => 1,
-												'title_li' => '',
-												'number'   => 10,
-											)
-										);
-										?>
-									</ul>
-
-								</div><!-- .widget -->
-
-							<?php endif; ?>
-
 							<?php
 
 							/* translators: %1$s: smiley */
